85580e6a795dc35fdbdc9b04dc1ddfcd6cc07cfc
name <karankarandevgan@gmail.com>
2025-03-27 11:23:39 +0530

    Solution done

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,17 +4,38 @@ import React from "react";
 import BookCard from "./components/BookCard";
 import books from "./booksData";
 import "./Home.css"; // Import the CSS for styling
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const goToForm = () => {
+    navigate("/Form");
+  };
+
   return (
-    <div className="home-container">
-      <h1>Book Library</h1>
-      <div className="books-grid">
-        {books.map((book) => (
-          <BookCard key={book.id} book={book} />
-        ))}
+    <>
+      <div className="home-container">
+        <button
+          style={{
+            position: "absolute",
+            right: "40px",
+            border: "1px solid black",
+            backgroundColor: "#FF8C00",
+            color: "white",
+          }}
+          onClick={goToForm}
+        >
+          Add Book
+        </button>
+        <h1>Book Library</h1>
+        <div className="books-grid">
+          {books.map((book) => (
+            <BookCard key={book.id} book={book} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,20 +1,21 @@
 // src/components/BookCard.jsx
 
 import React from "react";
+import { Link } from "react-router-dom";
 import "./BookCard.css"; // Import the CSS for styling
 
 const BookCard = ({ book }) => {
   const { title, author, description, coverImage } = book;
 
   return (
-    <div className="book-card">
+    <Link to={`/book/${book.id}`} className="book-card">
       <img src={coverImage} alt={`${title} cover`} className="book-cover" />
       <div className="book-details">
         <h2 className="book-title">{title}</h2>
         <h3 className="book-author">by {author}</h3>
         <p className="book-description">{description}</p>
       </div>
-    </div>
+    </Link>
   );
 };
 
